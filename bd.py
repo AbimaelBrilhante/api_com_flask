325d6cdcbf9d64ce6444dcdd9362d882d84ac1bb
Abimael Brilhante <88044370+AbimaelBrilhante@users.noreply.github.com>
2022-09-28 22:40:46 -0300

    update files

---
+++ bd.py
@@ -0,0 +1,26 @@
+Carros = [
+    {
+        'id':1,
+        'marca': 'Fiat',
+        'modelo':'Marea',
+        'ano':1999
+    },
+    {
+        'id': 2,
+        'marca': 'Volkswagen',
+        'modelo': 'Passat',
+        'ano': 1994
+    },
+    {
+        'id': 3,
+        'marca': 'Chevrolet',
+        'modelo': 'Monza',
+        'ano': 2000
+    },
+    {
+        'id': 4,
+        'marca': 'Ford',
+        'modelo': 'Ka',
+        'ano': 2009
+    },
+]
